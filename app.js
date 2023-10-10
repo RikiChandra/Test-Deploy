f59554cb69a8659aa6317df2f8f6a5e32b532611
RikiChandra <rikichandra37@gmail.com>
2023-10-10 20:28:14 +0700

    testing 1

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const PORT = 4000;
+const app = express()
+const PORT = 4000
 
 app.listen(PORT, () => {
-    console.log(`API listening on PORT ${PORT}`);
-});
+    console.log(`API listening on PORT ${PORT} `)
+})
 
 app.get('/', (req, res) => {
-    res.render('index'); // Menghilangkan indexRouter dari argumen res.render()
-});
+    res.send('Hey this is my API running 🥳')
+})
 
 app.get('/about', (req, res) => {
-    res.render('about');
-});
+    res.send('This is my about route..... ')
+})
 
-module.exports = app;
+// Export the Express API
+module.exports = app

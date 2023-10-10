d1a68361bb68948e965a1d345e415ea7d5b24339
RikiChandra <rikichandra37@gmail.com>
2023-10-10 20:25:07 +0700

    dep

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const PORT = 4000;
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORT, () => {
     console.log(`API listening on PORT ${PORT}`);

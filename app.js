9569f1f2621b8b489f1c07466e90127ecacc716c
RikiChandra <rikichandra37@gmail.com>
2023-10-10 20:42:15 +0700

    testing

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const express = require('express')
-const path = require('path')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express()
-const PORT = 4000
-app.set('view engine', 'jade');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.listen(PORT, () => {
-    console.log(`API listening on PORT ${PORT} `)
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Express' })
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.get('/about', (req, res) => {
-    res.send('This is my about route..... ')
-})
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
 
-// Export the Express API
-module.exports = app
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;

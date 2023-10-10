e6ceee2b950072c2bcfb87a24505e1a67f3fbf84
RikiChandra <rikichandra37@gmail.com>
2023-10-10 20:18:21 +0700

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = 4000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,6 +10,16 @@ var usersRouter = require('./routes/users');
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use('/', indexRouter);
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.render('index'); // Menghilangkan indexRouter dari argumen res.render()
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 module.exports = app;
